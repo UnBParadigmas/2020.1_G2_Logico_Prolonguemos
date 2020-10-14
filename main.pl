d2acb5dc576ae673728c6fc293afa2958a40a3c6
Guilherme Guy <guilhermeguy349@gmail.com>
2020-10-13 22:34:16 -0300

    feat: initial mongo connection

--- main.pl
+++ main.pl
@@ -1,3 +1,16 @@
-% :- use_module(mongo(mongo), []).
 
-loves(romeu, julieta).
+:- [lib/prolongo/load].
+:- use_module(mongo(mongo), []).
+
+main :- 
+    mongo:new_connection('localhost', 27017, Connection),
+    mongo:get_database(Connection, 'yelp', Database),
+    mongo:get_collection(Database, 'business', Collection),
+    mongo:find_one(Collection, [], Result),
+    print(Result),
+    mongo:free_connection(Connection).
+
+% bson:doc_get(Result, '_id', object_id(Id)).
+% bson:doc_get(Result, label, Label).
+% bson:doc_get(Result, priority, Priority).
+% format('~w~26|~w~45|~w~n', [Id,Label,Priority]).
