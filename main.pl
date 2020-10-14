3beb0494506d5994abc0f87e753e4c8f3042671a
Guilherme Guy <guilhermeguy349@gmail.com>
2020-10-14 20:29:30 -0300

    feat: getting all categories for a city

--- main.pl
+++ main.pl
@@ -6,9 +6,30 @@ main :-
     mongo:new_connection('localhost', 27017, Connection),
     mongo:get_database(Connection, 'yelp', Database),
     mongo:get_collection(Database, 'business', Collection),
-    mongo:find_one(Collection, [], Result),
-    print(Result),
+    mongo:find_all(
+        Collection,
+        [city-'Huntersville'],
+        [],
+        Docs
+    ),
+    print_cat(Docs),
     mongo:free_connection(Connection).
+
+
+
+print_cat([]).
+print_cat([Doc|Docs]) :-
+    bson:doc_get(Doc, categories, Categories),
+    get_categories(Categories, Value),
+    atomic_list_concat(SplitedCategories,',', Value),
+    writeln(SplitedCategories),
+    print_cat(Docs).
+
+get_categories(+null, Ret) :- Ret = ''.
+get_categories(Value, Ret) :- 
+    Ret = Value.
+
+
 
 % bson:doc_get(Result, '_id', object_id(Id)).
 % bson:doc_get(Result, label, Label).
